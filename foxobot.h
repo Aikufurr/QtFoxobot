9027bd85b9e9d6090c70936bd39b0130589a5570
Aikufurr <ikeybill1@gmail.com>
2021-02-04 00:49:25 +0000

    Update foxobot.h
    Changed create_slash_commands from slot to private

--- foxobot.h
+++ foxobot.h
@@ -21,12 +21,12 @@ private:
     QString application_id;
     QString dev_application_id;;
     QString dev_guild_id;
+    void create_slash_commands();
 
 private slots:
     void ready(QString name);
     void message_create(Client::message_t message);
     void interaction_create(Client::interaction_t *interaction);
-    void foxobot::create_slash_commands();
 };
 
 #endif // DISCORDBOT_H
